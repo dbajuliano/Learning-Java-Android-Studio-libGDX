a4a8508c20b916c18062833db223c90af84af203
Juliano tech <julianotech@hotmail.com>
2018-02-15 18:19:56 -0200

    Initial commit

---
+++ core/src/uk/jtech/game/jsnake/MainClass.java
@@ -0,0 +1,13 @@
+package uk.jtech.game.jsnake;
+
+import com.badlogic.gdx.Game;
+
+public class MainClass extends Game {
+
+
+    @Override
+    public void create() {
+        setScreen();
+
+    }
+}
