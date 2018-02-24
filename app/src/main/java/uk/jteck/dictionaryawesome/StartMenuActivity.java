d1e136aefd2e196c081f10a7cf8bdf368aac9b51
Juliano tech <julianotech@hotmail.com>
2018-02-24 19:35:55 -0300

    Initial commit

---
+++ app/src/main/java/uk/jteck/dictionaryawesome/StartMenuActivity.java
@@ -0,0 +1,24 @@
+package uk.jteck.dictionaryawesome;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_start_menu );
+    }
+
+    public void playTheGameClick(View view) {
+        //go to the DictionaryActivity
+    }
+
+    public void addANewWordClick(View view) {
+        Intent intent = new Intent(this, AddWordActivity.class);
+        startActivity( intent );
+    }
+}
