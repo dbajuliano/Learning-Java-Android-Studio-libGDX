cd02ace90730f10ef55bbe9fce0d6b932776b58a
Juliano tech <julianotech@hotmail.com>
2018-02-28 21:58:02 -0300

    Changed layout from linear to grid

--- app/src/main/java/uk/jteck/flagsactivity/FlagsActivity.java
+++ app/src/main/java/uk/jteck/flagsactivity/FlagsActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -36,28 +36,28 @@ public class FlagsActivity extends AppCompatActivity {
         setContentView( R.layout.activity_flags );
 
         // add a new button to the screen
-        Button butt = new Button (this);
-        butt.setText( "My Button" );
-        butt.setPadding( 30,10,30,10 );
+        //Button butt = new Button (this);
+        //butt.setText( "My Button" );
+        //butt.setPadding( 30,10,30,10 );
 
-        LinearLayout layout = (LinearLayout) findViewById( R.id.activity_flags );
+        GridLayout layout = findViewById( R.id.activity_flags );
 
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
         ViewGroup.LayoutParams.WRAP_CONTENT,
         ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        butt.setLayoutParams( params );
-        layout.addView( butt, 1 );
+        //butt.setLayoutParams( params );
+        //layout.addView( butt, 1 );
 
         for (String name : COUNTRIES){
             addFlag( name, layout );
         }
     }
 
-    private void addFlag(String countryName, LinearLayout layout){
+    private void addFlag(String countryName, GridLayout layout) {
         View flag = getLayoutInflater().inflate( R.layout.flag, layout );
 
-        TextView tv = (TextView) flag.findViewById(  R.id.flag_text);
+        TextView tv = flag.findViewById( R.id.flag_text );
         tv.setText( countryName );
 
         countryName = countryName.replace( " ", "" ).toLowerCase();
@@ -66,7 +66,7 @@ public class FlagsActivity extends AppCompatActivity {
         //R.drawable.unitedstates
         int flagImageID = getResources().getIdentifier( countryName, "drawable", getPackageName() );
 
-        ImageView img = (ImageView) flag.findViewById( R.id.flag_image );
+        ImageView img = flag.findViewById( R.id.flag_image );
         img.setImageResource( flagImageID );
 
 
