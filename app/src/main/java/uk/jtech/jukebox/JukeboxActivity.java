3532232efc9dd8dd551bdbefd4eb1a2290b1eae9
Juliano tech <julianotech@hotmail.com>
2018-03-04 19:09:11 -0300

    Setup Listener using StringArray and returned by log

--- app/src/main/java/uk/jtech/jukebox/JukeboxActivity.java
+++ app/src/main/java/uk/jtech/jukebox/JukeboxActivity.java
@@ -1,8 +1,11 @@
 package uk.jtech.jukebox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class JukeboxActivity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class JukeboxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_jukebox );
+
+        ListView list = findViewById( R.id.song_list );
+        list.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int index, long id) {
+                String[] songTitles = getResources().getStringArray( R.array.song_titles );
+                String song = songTitles[index];
+                Log.d( "result", "onItemClick: " + song );
+            }
+        } );
     }
 
     public void onClickPlay(View view) {
