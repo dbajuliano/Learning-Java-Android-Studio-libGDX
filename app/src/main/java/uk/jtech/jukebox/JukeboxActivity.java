017df7cc4ee342f9475ca94097d479b6e6b60e05
Juliano tech <julianotech@hotmail.com>
2018-03-05 03:00:48 -0300

    Deployed SPEECH TO TEXT (Mic OK)

--- app/src/main/java/uk/jtech/jukebox/JukeboxActivity.java
+++ app/src/main/java/uk/jtech/jukebox/JukeboxActivity.java
@@ -1,15 +1,21 @@
 package uk.jtech.jukebox;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import stanford.androidlib.SimpleActivity;
 
 public class JukeboxActivity extends SimpleActivity {
+    private static final int SPEECH_TO_TEXT_REQ_CODE = 1234;
 
     private MediaPlayer player = null;
     private TextToSpeech tts;
@@ -50,7 +56,7 @@ public class JukeboxActivity extends SimpleActivity {
         int songID = getResources().getIdentifier( song, "raw", getPackageName() );
 
         stopSong();
-        player = MediaPlayer.create( this, songID );
+        player = MediaPlayer.create( JukeboxActivity.this, songID );
         player.start();
 
         System.out.println( "log: " + song );
@@ -64,6 +70,31 @@ public class JukeboxActivity extends SimpleActivity {
     }
 
     public void onClickPlay(View view) {
+        Intent intent = new Intent( RecognizerIntent.ACTION_RECOGNIZE_SPEECH );
+        intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault() );
+        intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM );
+
+        // prompt text is shown on screen to tell user what say
+        intent.putExtra( RecognizerIntent.EXTRA_PROMPT, "Say a sentence here" );
+        startActivityForResult( intent, SPEECH_TO_TEXT_REQ_CODE );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult( requestCode, resultCode, intent );
+        if (requestCode == SPEECH_TO_TEXT_REQ_CODE) {
+            // I've come back from speech to text
+            ArrayList<String> list = intent.getStringArrayListExtra( RecognizerIntent.EXTRA_RESULTS );
+            if (list == null || list.isEmpty()) {
+                return;
+            }
+            String text = list.get( 0 );
+            for (int i = 1; i < list.size(); i++) {
+                text += " " + list.get( i );
+            }
+            log( text );
+        }
     }
 
     public void onClickStop(View view) {
