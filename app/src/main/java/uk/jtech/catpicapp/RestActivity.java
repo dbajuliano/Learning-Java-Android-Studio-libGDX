a38c50a819662f161c8b17babeb127a4ad23c667
Juliano tech <julianotech@hotmail.com>
2018-03-06 18:30:11 -0300

    JSONArray, img url, and log url has been deployed

--- app/src/main/java/uk/jtech/catpicapp/RestActivity.java
+++ app/src/main/java/uk/jtech/catpicapp/RestActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import stanford.androidlib.SimpleActivity;
+import stanford.androidlib.xml.XML;
 
 public class RestActivity extends SimpleActivity {
 
@@ -40,14 +42,48 @@ public class RestActivity extends SimpleActivity {
                             JSONObject value = json.getJSONObject( "value" );
                             String joke = value.getString( "joke" );
                             $TV( R.id.output ).setText( joke );
-                        }
-                        catch (JSONException jsone){
-                            Log.wtf("help", jsone);
+                        } catch (JSONException jsone) {
+                            Log.wtf( "help", jsone );
                         }
                     }
                 } );
     }
 
-    public void catPicsClick(View view) {
+    public void catClick(View view) {
+
+        Ion.with( this )
+                .load( "http://thecatapi.com/api/images/get?format=xml&results_per_page=6" )
+                .asString()
+                .setCallback( new FutureCallback<String>() {
+                    // {
+                    //   "type": "success",
+                    //   "value": {
+                    //              "id": 268,
+                    //               "joke": "Time waits for no man. Unless that man is Chuck Norris."
+                    //       }
+                    // }
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        //data has arrived
+                        try {
+                            JSONObject json = XML.toJSONObject( result );
+                            log( json );
+
+                            JSONArray a = json.getJSONObject( "response" )
+                                    .getJSONObject( "data" )
+                                    .getJSONObject( "images" )
+                                    .getJSONArray( "image" );
+                            for (int i = 0; i < a.length(); i++) {
+                                JSONObject img = a.getJSONObject( i );
+                                String url = img.getString( "url" );
+                                log( url );
+                            }
+                        } catch (JSONException jsone) {
+                            Log.wtf( "help", jsone );
+                        }
+                    }
+                } );
+
+
     }
 }
