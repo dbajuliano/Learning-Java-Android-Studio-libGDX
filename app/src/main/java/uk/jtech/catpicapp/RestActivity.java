b28bb7e4b2c531cf06e40cf54070a8928e8248f5
Juliano tech <julianotech@hotmail.com>
2018-03-06 18:44:33 -0300

    Added ImageView with Picasso

--- app/src/main/java/uk/jtech/catpicapp/RestActivity.java
+++ app/src/main/java/uk/jtech/catpicapp/RestActivity.java
@@ -3,9 +3,13 @@ package uk.jtech.catpicapp;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -50,6 +54,8 @@ public class RestActivity extends SimpleActivity {
     }
 
     public void catClick(View view) {
+        GridLayout grid = $( R.id.grid );
+        grid.removeAllViews();
 
         Ion.with( this )
                 .load( "http://thecatapi.com/api/images/get?format=xml&results_per_page=6" )
@@ -77,13 +83,27 @@ public class RestActivity extends SimpleActivity {
                                 JSONObject img = a.getJSONObject( i );
                                 String url = img.getString( "url" );
                                 log( url );
+                                loadImage( url );
                             }
                         } catch (JSONException jsone) {
                             Log.wtf( "help", jsone );
                         }
                     }
                 } );
+    }
 
-
+    public void loadImage(String url) {
+        ImageView imgView = new ImageView( this );
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        imgView.setLayoutParams( params );
+        GridLayout grid = $( R.id.grid );
+        grid.addView( imgView );
+        Picasso.with( this )
+                .load( url )
+                .resize( 400, 300 )
+                .into( imgView );
     }
 }
